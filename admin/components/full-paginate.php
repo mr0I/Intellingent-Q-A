862aa745e4f295bdd1f068670f69152341e209f2
ali <wizard2070@gmail.com>
2022-12-28 23:14:27 +0330

    minor_improvements

--- admin/components/full-paginate.php
+++ admin/components/full-paginate.php
@@ -3,22 +3,22 @@
 <?php
 $prevUrl = sprintf("&p=%u", $page - 1);
 $firstUrl = sprintf("&p=%u", 1);
-$whichHalf = (abs($items->current_page - $items->last_page) > abs($items->current_page - 1))
+$whichHalf = (abs($items->metadata->current_page - $items->metadata->last_page) > abs($items->metadata->current_page - 1))
     ? 'firstHalf'
     : 'secondHalf';
-$lastUrl = sprintf("&p=%u", $items->last_page);
+$lastUrl = sprintf("&p=%u", $items->metadata->last_page);
 $nextUrl = sprintf("&p=%u", $page + 1);
 ?>
 
 
 <div class="pagination">
     <ul class="pagination">
-        <?php if ($items->current_page != 1) : ?>
-            <li class="page-item <?= ($items->current_page == 1) ? 'disabled' : ''; ?>">
+        <?php if ($items->metadata->current_page != 1) : ?>
+            <li class="page-item <?= ($items->metadata->current_page == 1) ? 'disabled' : ''; ?>">
                 <a href="<?= $pagePath . $prevUrl ?>" tabindex="-1"><?= __('Previous', 'intl_qa_lan') ?></a>
             </li>
         <?php endif; ?>
-        <li class="page-item <?= ($items->current_page == 1) ? 'active' : ''; ?>">
+        <li class="page-item <?= ($items->metadata->current_page == 1) ? 'active' : ''; ?>">
             <a href="<?= $pagePath . $firstUrl ?>">1</a>
         </li>
         <?php if ($whichHalf === 'secondHalf') : ?>
@@ -28,10 +28,10 @@ $nextUrl = sprintf("&p=%u", $page + 1);
         <?php endif; ?>
 
         <?php
-        for ($i = 2; $i < $items->last_page; $i++) :
-            if (abs($items->current_page - $i) < 3) :
+        for ($i = 2; $i < $items->metadata->last_page; $i++) :
+            if (abs($items->metadata->current_page - $i) < 3) :
                 $pageUrl = sprintf('&p=%u', $i); ?>
-                <li class="page-item <?= ($items->current_page == $i) ? 'active' : ''; ?>">
+                <li class="page-item <?= ($items->metadata->current_page == $i) ? 'active' : ''; ?>">
                     <a href="<?= $pagePath . $pageUrl ?>"><?= $i; ?></a>
                 </li>
         <?php endif;
@@ -42,12 +42,12 @@ $nextUrl = sprintf("&p=%u", $page + 1);
                 <span>...</span>
             </li>
         <?php endif; ?>
-        <li class="page-item <?= ($items->current_page == $items->last_page) ? 'active' : ''; ?>">
+        <li class="page-item <?= ($items->metadata->current_page == $items->metadata->last_page) ? 'active' : ''; ?>">
             <a href="<?= $pagePath . $lastUrl ?>">
-                <?= $items->last_page; ?>
+                <?= $items->metadata->last_page; ?>
             </a>
         </li>
-        <?php if ($items->current_page != $items->last_page) : ?>
+        <?php if ($items->metadata->current_page != $items->metadata->last_page) : ?>
             <li class="page-item">
                 <a href="<?= $pagePath . $nextUrl ?>"><?= __('Next', 'intl_qa_lan') ?></a>
             </li>
--- inc/controllers/ReportsController.php
+++ inc/controllers/ReportsController.php
@@ -10,9 +10,10 @@ function getReports($offset, $limit)
     $reports =  $wpdb->get_results(" SELECT * FROM ${reportsTable} ORDER BY updated_at DESC LIMIT ${offset},${limit}");
 
     $result = new stdClass();
+    $result->metadata = new stdClass();
     $result->data = $reports;
-    $result->last_page = ceil(intval($reportsCount) / $limit);
-    $result->current_page = ($offset / $limit) + 1;
+    $result->metadata->last_page = ceil(intval($reportsCount) / $limit);
+    $result->metadata->current_page = ($offset / $limit) + 1;
 
     return $result;
 }
